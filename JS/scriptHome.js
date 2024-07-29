5ffa88a122b155e4471a56f652682c586b5cb0c8
Arthur Souza Sepp <arthursouzasepp@apoioprodesp.sp.gov.br>
2024-07-29 14:03:55 -0300

    Commit inicial

---
+++ JS/scriptHome.js
@@ -0,0 +1,20 @@
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdown-content");
+    if (dropdownContent.classList.contains("show")) {
+        dropdownContent.classList.remove("show");
+    } else {
+        dropdownContent.classList.add("show");
+    }
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
