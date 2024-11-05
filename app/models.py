bd6def3bd7b704acdc02c0f5afc3a84e7ee77a2d
Arthur Souza Sepp <arthursouzasepp@apoioprodesp.sp.gov.br>
2024-11-05 09:40:58 -0300

    Definindo modelos base para o funcionamento do sistema.

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class Unidade(models.Model):
     cidade = models.CharField(max_length=100)
     telefone = models.CharField(max_length=11)
     endereco = models.CharField(max_length=150)
-    cursos = models.ManyToManyField('Curso', related_name='unidades', blank=True)
+    cursos = models.ManyToManyField('Curso', related_name='unidades_curso', blank=True)  # Ajuste do related_name
     
     def __str__(self):
         return f'Fatec {self.cidade}'
@@ -24,10 +24,9 @@ class Curso(models.Model):
     nome = models.CharField(max_length=150)
     periodo_choices = [('MANHA', 'Manhã'), ('TARDE', 'Tarde'), ('NOITE', 'Noite')]
     periodo = models.CharField(max_length=5, choices=periodo_choices)
-    unidade = models.ManyToManyField('Unidade', related_name='cursos', blank=True)
     duracao_anos = models.PositiveIntegerField()
     duracao_semestres = models.PositiveIntegerField()
-    materias = models.ManyToManyField('Materia', related_name='cursos', blank=True)
+    materias = models.ManyToManyField('Materia', related_name='cursos_materia', blank=True)  # Ajuste do related_name
     
     def validar_duracao(self):
         semestres = self.duracao_semestres
@@ -39,8 +38,8 @@ class Curso(models.Model):
 
 class Materia(models.Model):
     nome = models.CharField(max_length=100)
-    professor = models.ForeignKey('Professor', on_delete=models.CASCADE, related_name='materia', blank=True, null=True)
-    curso = models.ForeignKey('Curso', on_delete=models.CASCADE, related_name='materias', blank=True, null=True)
+    professor = models.ForeignKey('Professor', on_delete=models.CASCADE, related_name='materias_professor', blank=True, null=True)  # Ajuste do related_name
+    curso = models.ForeignKey('Curso', on_delete=models.CASCADE, related_name='materias_curso', blank=True, null=True)  # Ajuste do related_name
     
     def __str__ (self):
         return self.nome
@@ -51,8 +50,8 @@ class Professor(models.Model):
     cpf = models.CharField(max_length=11)
     matricula = models.CharField(max_length=13)
     email = models.EmailField()
-    curso = models.ManyToManyField('Curso', related_name='professores', blank=True)
-    materia = models.ManyToManyField('Materia', related_name='professores', blank=True)
+    curso = models.ManyToManyField('Curso', related_name='professores_curso', blank=True)  # Ajuste do related_name
+    materia = models.ManyToManyField('Materia', related_name='professores_materia', blank=True)  # Ajuste do related_name
     
     def __str__(self):
         return f'{self.nome} {self.sobrenome}'
@@ -63,7 +62,7 @@ class Aluno(models.Model):
     cpf = models.CharField(max_length=11)
     matricula = models.CharField(max_length=13)
     email = models.EmailField()
-    curso = models.ForeignKey('Curso', on_delete=models.CASCADE, related_name='alunos', blank=True, null=True)
+    curso = models.ForeignKey('Curso', on_delete=models.CASCADE, related_name='alunos_curso', blank=True, null=True)  # Ajuste do related_name
     
     def __str__(self):
         return f'{self.nome} {self.sobrenome}'
