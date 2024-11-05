5c6e4cb5b76808bc397f7fa782cdce5c204921d6
Arthur Souza Sepp <arthursouzasepp@apoioprodesp.sp.gov.br>
2024-11-05 09:24:26 -0300

    Adicionando models ao admin

--- app/admin.py
+++ app/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Aluno)
+admin.site.register(Professor)
+admin.site.register(Materia)
+admin.site.register(Curso)
+admin.site.register(Unidade)
+admin.site.register(Aviso)
