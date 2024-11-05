90ea0c3eb0db245c661ca87afa13a6224932dd89
Arthur Souza Sepp <arthursouzasepp@apoioprodesp.sp.gov.br>
2024-11-05 08:29:02 -0300

    Reiniciando projeto

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import *
 
-admin.site.register(Aluno),
-admin.site.register(Aviso),
-admin.site.register(Curso),
-admin.site.register(Materia),
-admin.site.register(Professor),
-admin.site.register(Unidade),
-admin.site.register(Secretaria),
+# Register your models here.
--- app/views.py
+++ app/views.py
@@ -1,29 +1,3 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
-def index(request):
-    return redirect('login')
-
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('profile')  # Redireciona para a página de perfil
-            else:
-                messages.error(request, 'Usuário ou senha inválidos.')
-        else:
-            messages.error(request, 'Usuário ou senha inválidos.')
-    form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
-
-@login_required
-def home(request):
-    return render(request, 'home.html')
+# Create your views here.
